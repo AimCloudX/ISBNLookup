74fa6389170e629ecf8cca62c9f1eef4fb04d741
Shigekazu Koya <shigekazukoya@gmail.com>
2024-09-26 22:36:42 +0900

    Componentsが上下にならないように修正

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,20 @@ function App() {
   const [isbns, setIsbns] = useState<string>('');
 
   return (
-    <div className="bg-gradient-to-br from-orange-100 to-orange-300 h-screen flex flex-col">
+    <div className="bg-gradient-to-br from-orange-100 to-orange-300 min-h-screen flex flex-col">
       <header className="bg-white shadow-md flex-none">
         <div className="container mx-auto px-6 py-4">
           <h1 className="text-4xl font-bold text-center text-orange-700">📚 ISBN検索ツール</h1>
         </div>
       </header>
 
-      <div className="flex-grow overflow-hidden">
-        <main className="mx-auto px-6 py-4 h-full flex">
-          <div className="flex flex-col lg:flex-row lg:space-x-6 h-full w-full">
-            <div className="lg:w-1/2 h-full flex flex-col">
+      <div className="flex-grow overflow-x-auto">
+        <main className="container mx-auto px-6 py-4 h-full">
+          <div className="flex flex-row space-x-6 flex-nowrap">
+            <div className="min-w-[300px] lg:min-w-1/2 h-full flex flex-col">
               <KeywordSearch setIsbns={setIsbns} />
             </div>
-            <div className="lg:w-1/2 h-full flex flex-col">
+            <div className="min-w-[300px] lg:min-w-1/2 h-full flex flex-col">
               <ISBNSearch isbns={isbns} setIsbns={setIsbns} />
             </div>
           </div>
@@ -29,7 +29,7 @@ function App() {
 
       <footer className="bg-white shadow-inner flex-none">
         <div className="container mx-auto px-6 py-4 text-center text-gray-600">
-          &copy; 2024 ISBN検索ツール
+          &copy; 2024 AimCloudX
         </div>
       </footer>
     </div>
